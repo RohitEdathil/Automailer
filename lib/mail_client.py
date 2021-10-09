55c4b9f4dbbbd09b38e858c23f5e5c28978bf0bd
Gokul-Krishnan-kp <84298547+Gokul-Krishnan-kp@users.noreply.github.com>
2021-10-08 19:59:39 -0700

    Done , I guess :)

--- lib/mail_client.py
+++ lib/mail_client.py
@@ -1,11 +1,17 @@
-
 class MailClient():
-    """MailClient provides an easy to use interface to send emails."""
+ 
 
-    def __init__(self, email: str, password: str) -> None:
-        """Takes in email and password and logs into the server"""
-        pass
+  def __init__(self, email: str, password: str):
+    import yagmail
+    self.email = email
+    self.password = password
+    yag = yagmail.SMTP(self.email)
+    yag = yagmail.SMTP(user=self.email, password=self.password)
 
-    def send(self, to: str, subject: str, content: str) -> None:
-        """Sends an email to the specified address"""
-        pass
+  def send(self, to: str, subject: str, content: str):
+    import yagmail
+    yag = yagmail.SMTP(self.email)
+    self.to = to
+    self.subject = subject
+    self.content = content
+    yag.send(self.to, self.subject, self.content)
