a9f2a22ab852b4020f294e0eaea97cd567004bd7
Rohit V <rohitedathil@gmail.com>
2021-10-09 15:34:19 +0530

    added docstrings and minor optimizations

--- lib/mail_client.py
+++ lib/mail_client.py
@@ -1,17 +1,13 @@
+import yagmail
+
+
 class MailClient():
-  
-  def __init__(self, email: str, password: str):
-    import yagmail
-    self.email = email
-    self.password = password
-    yag = yagmail.SMTP(self.email)
-    yag = yagmail.SMTP(user=self.email, password=self.password)
+    """MailClient provides an easy to use interface to send emails."""
 
-  def send(self, to: str, subject: str, content: str):
-    import yagmail
-    yag = yagmail.SMTP(self.email)
-    self.to = to
-    self.subject = subject
-    self.content = content
-    yag.send(self.to, self.subject, self.content)
+    def __init__(self, email: str, password: str):
+        """Takes in email and password and logs into the server"""
+        self.yag = yagmail.SMTP(user=email, password=password)
 
+    def send(self, to: str, subject: str, content: str):
+        """Sends an email to the specified address"""
+        self.yag.send(to, subject, content)
