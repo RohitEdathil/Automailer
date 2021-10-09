e1cdae0d5c4602be42d7998b169ac64d7e3b0e2c
Rohit V <rohitedathil@gmail.com>
2021-10-09 20:21:39 +0530

    testing complete

--- lib/renderer.py
+++ lib/renderer.py
@@ -4,7 +4,7 @@ from jinja2 import Template
 class TemplateRenderer():
     """Provides an easy interface to the templating engine"""
 
-    def _init_(self, template: str) -> None:
+    def __init__(self, template: str) -> None:
         """Instantiates the engine with the specified template"""
         self.template: Template = Template(open(template, "r").read())
 
